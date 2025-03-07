70373b5890e28e5b797c95b18da92cb9f8f069af
Maksymilian Palkowski <s30792@pjwstk.edu.pl>
2025-03-07 16:43:47 +0100

    Modyfikacja 3

--- Compton/Compton/Program.cs
+++ Compton/Compton/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Echa historii dziś słychać jak nigdy");
 Console.WriteLine("Przysięgam na boga że nie przegram na bitwy");
+Console.WriteLine("Siema [FIKCYJNA POSTACIO] nieciekawy typku");
