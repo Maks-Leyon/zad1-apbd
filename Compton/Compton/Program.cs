7022d355ae81cf459463fddf8a5e26cd56b7d87e
Maksymilian Palkowski <s30792@pjwstk.edu.pl>
2025-03-07 16:42:46 +0100

    Modyfikacja 2

--- Compton/Compton/Program.cs
+++ Compton/Compton/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Echa historii dziś słychać jak nigdy");
+Console.WriteLine("Przysięgam na boga że nie przegram na bitwy");
